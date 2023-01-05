8408a6a3ffe40b9cdaa44ef3021ca15db0f59b9b
Georgi Ginev <gosho.ginev@gmail.com>
2023-01-05 18:28:27 +0200

    movie form update

--- src/app/modules/panel/movie/form/form.component.ts
+++ src/app/modules/panel/movie/form/form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Movie } from 'src/app/shared/resources/movies/movie.service';
+import { FormService } from 'src/app/shared/services/form/form.service';
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
+import { PanelPageService } from '../../panel-page.service';
 
 @Component({
   selector: 'app-form',
@@ -6,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  public movie: Movie;
+  public formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private panelPageService: PanelPageService,
+    private formService: FormService,
+    private toastService: ToastService,
+    private router: Router
+  ) { 
+    this.createForm();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(this.movie) {
+      this.panelPageService.updateHeader('Edit Movie')
+    } else {
+      this.panelPageService.updateHeader('Add Movie')
+    }
+  }
 
+  public create() {
+    this.formService.isValid(this.formGroup).then(() => {
+      this.toastService.success({
+        header: 'The movie was successfully added',
+      });
+
+      this.router.navigate(['/panel/movies']);
+    }, () => {})
+  }
+
+  private createForm() {
+    this.formGroup = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      description: [null, [Validators.maxLength(200)]],
+      image: [null],
+      background_image: [null]
+    });
+  }
 }
